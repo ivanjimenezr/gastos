2e16b109815b609369c514724a03974f544329ca
chiklete <chiklete@gmail.com>
2025-03-17 17:11:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-hello word
+import sys
+from os.path import dirname, abspath
+
+sys.path.append(dirname(abspath(__file__)))
+
+from main import app
+from fastapi.middleware.wsgi import WSGIMiddleware
+
+application = WSGIMiddleware(app)
