5d27b8a9f054eda193ca5abcc8c068328486f276
chiklete <chiklete@gmail.com>
2025-03-18 09:16:18 +0100

    Update all_bills.py

--- all_bills.py
+++ all_bills.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, APIRouter
+from pymongo import MongoClient
 
 router = APIRouter()
 app = FastAPI()
 
-@router.post("/all_bills")
+@router.get("/all_bills")
 def process_data(): 
    return {"message": "all_bills, el proceso está en curso."}
