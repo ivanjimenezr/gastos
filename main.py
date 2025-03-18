f20a0b1244430c598b6680c1ed168f50183cd45b
chiklete <chiklete@gmail.com>
2025-03-18 09:08:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
-import all_bills
+import all_bills, add_bills
 
 app = FastAPI()
 
 app.include_router(all_bills.router, tags=["bills"])
+app.include_router(add_bills.router, tags=["bills"])
 
 @app.get("/")
 async def read_root():
