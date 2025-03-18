45265ec8813acd41113837dc5983bca7cdf56cd8
chiklete <chiklete@gmail.com>
2025-03-18 09:11:30 +0100

    Update add_bills.py

--- add_bills.py
+++ add_bills.py
@@ -3,6 +3,33 @@ from fastapi import FastAPI, APIRouter
 router = APIRouter()
 app = FastAPI()
 
+examples_responses = {
+    200: {
+     "description": "Successful Response",
+     "content": {
+         "application/json": {
+             "example": {"message": "Item created successfully", "item_id": 123}
+         }
+     },
+ },
+ 402: {
+     "description": "Validation Error",
+     "content": {
+         "application/json": {
+             "example": {
+                 "detail": [
+                     {
+                         "loc": ["body", "name"],
+                         "msg": "field required",
+                         "type": "value_error.missing"
+                     }
+                 ]
+             }
+         }
+     },
+ },
+}
+
 @router.post("/add_bills", responses=examples_responses)
 def process_data(event): 
     print(f"event recibido:")
